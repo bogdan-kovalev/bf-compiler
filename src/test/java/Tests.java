d8954694015366f28ac45e74c6d6342970422c7b
Bogdan Kovalev <botinax@ukr.net>
2014-10-09 15:11:33 +0300

    added: testExecuter

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,4 +1,10 @@
+import bvm.*;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -8,32 +14,48 @@ import static org.junit.Assert.*;
 public class Tests {
     @Test
     public void testInterpreter() {
-        String code = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++." +
+        String testCode = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++." +
                 ".+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
 
         String expected = "Hello World!\n";
-        String actual = new BrainfuckInterpreter().interpret(code);
+        String actual = new BrainfuckInterpreter().interpret(testCode);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExecuter() {
+        List<Command> testList = new ArrayList<>();
+        testList.add(new IncrementPointerCommand());
+        testList.add(new DecrementPointerCommand());
+        testList.add(new IncrementCellCommand());
+        testList.add(new PrintCellCommand());
+        testList.add(new DecrementCellCommand());
+        testList.add(new Cycle());
+
+        String expected = String.valueOf((char)1);
+        String actual = new CommandsExecuter().run(testList);
 
         assertEquals(expected, actual);
     }
 
     @Test
     public void testCompiler() {
-        String code = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++." +
+        String testCode = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++." +
                 ".+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
 
         String expected = "Hello World!\n";
-        String actual = new CommandsExecuter().run(new BrainfuckCompiler().compile(code));
+        String actual = new CommandsExecuter().run(new BrainfuckCompiler().compile(testCode));
 
         assertEquals(expected, actual);
     }
 
     @Test
     public void testLoopsOnCompiler() {
-        String code = "++++++++[>+++++++[>+<-]<-]>>.";
+        String testCode = "++++++++[>+++++++[>+<-]<-]>>.";
 
         String expected = String.valueOf((char)56);
-        String actual = new CommandsExecuter().run(new BrainfuckCompiler().compile(code));
+        String actual = new CommandsExecuter().run(new BrainfuckCompiler().compile(testCode));
 
         assertEquals(expected, actual);
     }
